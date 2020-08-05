a897d8ee529d6bdd7b8c720c84b5a7c77bb3092a
Sriya <tsriya6@gmail.com>
2020-08-05 18:47:18 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   useEffect(() => {
     fetch(
-      `https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${term}&image_type=vector&pretty=true`
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${term}&image_type=illustration&pretty=true`
     )
       .then((res) => res.json())
       .then((data) => {
